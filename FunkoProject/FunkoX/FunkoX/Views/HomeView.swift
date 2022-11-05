1a61a903c7ad225e194e15be4ff01ca5c3b6f2c9
Austin <austinwheeler1112@gmail.com>
2022-11-05 03:16:10 -0400

    Adding finished swipable top interface

--- FunkoProject/FunkoX/FunkoX/Views/HomeView.swift
+++ FunkoProject/FunkoX/FunkoX/Views/HomeView.swift
@@ -8,14 +8,14 @@
 import SwiftUI
 
 struct HomeView: View {
-    private var numberOfImages = 5
-    private let timer = Timer.publish(every: 3, on: .main, in: .common).autoconnect()
+    private let numberOfImages = 5
+    private let timer = Timer.publish(every: 5, on: .main, in: .common).autoconnect()
     @State private var currentIndex = 0
     var body: some View {
         GeometryReader { proxy in
-            TabView {
+            TabView(selection: $currentIndex) {
                 ForEach(0..<numberOfImages) { num in
-                    Image("Image")
+                    Image("0")
                         .resizable()
                         .scaledToFit()
                         .overlay(Color.black.opacity(0.1))
@@ -26,7 +26,9 @@ struct HomeView: View {
                 .padding(.bottom)
                 .frame(width: proxy.size.width, height: proxy.size.height/3)
                 .onReceive(timer, perform: { _ in
-                    currentIndex = currentIndex < numberOfImages ? currentIndex + 1 : 0
+                    withAnimation {
+                        currentIndex = currentIndex < numberOfImages ? currentIndex + 1 : 0
+                    }
                 })
         }
     }
