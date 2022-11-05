f9dd92dfc2e723461825e4f50a1495e92b743e8d
Austin <austinwheeler1112@gmail.com>
2022-11-05 04:34:00 -0400

    Added base code for Three categories with horizontal scroll wheels

---
+++ FunkoProject/FunkoX/FunkoX/Views/CategoryView.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryView.swift
+//  FunkoX
+//
+//  Created by Austin Wheeler on 11/5/22.
+//
+
+import SwiftUI
+
+struct CategoryView: View {
+    var body: some View {
+        HomeView()
+        HStack {
+            Text("jjj")
+        }
+    }
+}
+
+struct CategoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryView()
+    }
+}
